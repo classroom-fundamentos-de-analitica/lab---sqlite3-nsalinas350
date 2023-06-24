2aa83ad1c61811e5ffcd1dfbff32d499872ab9b9
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 20:30:49 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,9 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
+SELECT tbl1.K0, AVG(tbl2.c21) AS avg_c21
+    FROM tbl1
+    JOIN tbl2 ON tbl1.K1 = tbl2.K1
+    WHERE tbl1.c13 > 400
+    GROUP BY tbl1.K0
 
